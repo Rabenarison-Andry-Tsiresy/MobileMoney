626f4f793dceeea25926bb7671dd89335783e6f9
Rabenarison-Andry-Tsiresy <trabenarison@gmail.com>
2026-07-20 17:30:44 +0300

    go

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,8 +5,6 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-
 
 // ====================================================================
 // MODULE ADMINISTRATEUR (Côté Opérateur)
@@ -42,3 +40,47 @@ $routes->get('tarif/delete/(:num)',     'TarifController::delete/$1');
 // --- Routes pour les Rapports ---
 $routes->get('rapport/gains',           'RapportController::gains');
 $routes->get('rapport/compte',         'RapportController::comptesClients');
+
+
+$routes->get('/', 'AuthController::login');
+
+$routes->group('Authentification', function ($routes) {
+    $routes->get('/', 'AuthController::login');
+    $routes->get('login', 'AuthController::login');
+    $routes->post('loginAuth', 'AuthController::loginAuth');
+    $routes->get('logout', 'AuthController::logout');
+});
+
+// Compte
+$routes->group('Compte', function ($routes) {
+    $routes->get('solde', 'CompteController::solde');
+    $routes->get('historique', 'CompteController::historique');
+});
+
+// Transactions
+$routes->group('Transaction', function ($routes) {
+    // Dépôt
+    $routes->get('depot', 'TransactionController::depot');
+    $routes->post('depot', 'TransactionController::faireDepot');
+    
+    // Retrait
+    $routes->get('retrait', 'TransactionController::retrait');
+    $routes->post('retrait', 'TransactionController::faireRetrait');
+    
+    // Transfert
+    $routes->get('transfert', 'TransactionController::transfert');
+    $routes->post('transfert', 'TransactionController::faireTransfert');
+});
+
+// Dashboard
+$routes->get('dashboard', 'DashboardController::index');
+
+// Commissions
+$routes->group('Commission', function ($routes) {
+    $routes->get('/', 'CommissionController::index');
+    $routes->get('create', 'CommissionController::create');
+    $routes->post('store', 'CommissionController::store');
+    $routes->get('edit/(:num)', 'CommissionController::edit/$1');
+    $routes->post('update/(:num)', 'CommissionController::update/$1');
+    $routes->get('delete/(:num)', 'CommissionController::delete/$1');
+});
